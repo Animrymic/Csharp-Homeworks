5978196dd46ca42dd32567731de25977574045c9
Animrymic <trojanovikdragan@gmail.com>
2026-03-19 14:29:03 +0100

    all exercises added

--- Class02.Hworks/Class02/Class02.Homeworks/Program.cs
+++ Class02.Hworks/Class02/Class02.Homeworks/Program.cs
@@ -1,6 +1,8 @@
 ﻿#region Exercise 1
 
 //DOUBLE VARIABLES
+using System.Security.Principal;
+
 double doubleNum1 = 9;
 double doubleNum2 = 3;
 double doubleResult = doubleNum1 / doubleNum2;
@@ -42,5 +44,91 @@ string textValue = "The number is: ";
 string resultText = textValue + numberValue;
 
 Console.WriteLine(resultText);
+
+#endregion
+
+#region Exercise 3
+
+int creditAmount = 102;
+int smsCount = 5;
+
+int numberOfMessages = creditAmount / smsCount;
+
+Console.WriteLine("You can send " + numberOfMessages + " SMS messages.");
+
+#endregion
+
+#region Exercise 4
+
+int applesPerBranch = 8;
+int branchesPerTree = 12;
+int basketCapacity = 5;
+
+Console.Write("Enter a number of trees: ");
+int numberOfTrees = int.Parse(Console.ReadLine());
+
+int totalApples = numberOfTrees * branchesPerTree * applesPerBranch;
+
+int basketsNeeded = totalApples / basketCapacity;
+
+if (totalApples % basketCapacity != 0)
+{
+    basketsNeeded++;
+}
+
+Console.WriteLine("Total apples: " + totalApples);
+Console.WriteLine("Total baskets: " + basketsNeeded);
+
+#endregion
+
+#region Exercise 5
+
+int firstNumberValue = 12;
+int secondNumberValue = 7;
+
+int largeNumberValue; 
+
+if(firstNumberValue > secondNumberValue)
+{
+    largeNumberValue = firstNumberValue; 
+}
+else
+{
+    largeNumberValue = secondNumberValue; 
+}
+
+if(largeNumberValue % 2 == 0)
+{
+    Console.WriteLine("The larger number from the two is: " + largeNumberValue);
+    Console.WriteLine("And the number is even");
+}
+else
+{
+    Console.WriteLine("The larger number from the two is: " + largeNumberValue);
+    Console.WriteLine("And the number is odd");
+}
+#endregion
+
+#region Exercise 6
+
+Console.Write("Enter a number from 1 to 3: ");
+int userChoiceNumber = int.Parse(Console.ReadLine()); 
+
+if (userChoiceNumber == 1)
+{
+    Console.WriteLine("You got a new car!");
+}
+else if (userChoiceNumber == 2)
+{
+    Console.WriteLine("You got a new plane!");
+}
+else if (userChoiceNumber == 3)
+{
+    Console.WriteLine("You got a new bike!");
+}
+else
+{
+    Console.WriteLine("Error: Invalid input");
+}
 
 #endregion
