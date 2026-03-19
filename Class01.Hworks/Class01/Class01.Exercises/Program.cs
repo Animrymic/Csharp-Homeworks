221495120ff13923340ff1c3c3cc8d482de7fabb
Animrymic <trojanovikdragan@gmail.com>
2026-03-19 13:58:31 +0100

    exercises 2 and 3 added

--- Class01.Hworks/Class01/Class01.Exercises/Program.cs
+++ Class01.Hworks/Class01/Class01.Exercises/Program.cs
@@ -1,7 +1,20 @@
-﻿Console.WriteLine("    *");
+﻿#region Exercise 1
+
+Console.WriteLine("    *");
 Console.WriteLine("   ***");
 Console.WriteLine("  *****");
 Console.WriteLine(" *******");
 Console.WriteLine("*********");
 
+#endregion
+
+#region Exercise 2 
+
+Console.WriteLine("     *");
+Console.WriteLine("    ***");
+Console.WriteLine("     *");
+Console.WriteLine("    * *");
+Console.WriteLine("   *   *");
+
+#endregion
 
