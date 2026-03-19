a47dda88f69c5cd20186300a2273a72d1dc55657
Animrymic <trojanovikdragan@gmail.com>
2026-03-19 14:06:50 +0100

    exercises 1 and 2 added

--- Class02.Hworks/Class02/Class02.Homeworks/Program.cs
+++ Class02.Hworks/Class02/Class02.Homeworks/Program.cs
@@ -1,2 +1,46 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine(      
+﻿#region Exercise 1
+
+//DOUBLE VARIABLES
+double doubleNum1 = 9;
+double doubleNum2 = 3;
+double doubleResult = doubleNum1 / doubleNum2;
+
+Console.WriteLine("Double division result: " + doubleResult);
+
+// INT VARIABLES
+int intNum1 = 9;
+int intNum2 = 3;
+int intResult = intNum1 / intNum2;
+
+Console.WriteLine("Int division result: " + intResult);
+
+// COMPARISON
+Console.WriteLine("Are results equal? " + (doubleResult == intResult));
+
+// STRING CONCATENATION
+string firstText = "Hello ";
+string secondText = "World";
+string combinedText = firstText + secondText;
+
+Console.WriteLine("Concatenated string: " + combinedText);
+
+// STRING NUMBERS CONCATENATION
+string stringNine = "9";
+string stringThree = "3";
+string concatenatedNumbers = stringNine + stringThree;
+
+Console.WriteLine("Concatenated numbers as string: " + concatenatedNumbers);
+
+
+#endregion
+
+#region Exercise 2
+
+int numberValue = 7;
+string textValue = "The number is: ";
+
+string resultText = textValue + numberValue;
+
+Console.WriteLine(resultText);
+
+#endregion
