a5fbd507dde985974b1c8764c9f571419aac53e1
Animrymic <trojanovikdragan@gmail.com>
2026-03-19 14:52:59 +0100

    exercise 1 added

--- Class03.Hworks/Class03/Class03.Exercises/Program.cs
+++ Class03.Hworks/Class03/Class03.Exercises/Program.cs
@@ -1,1 +1,59 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+#region Exercise 1
+
+// Count up to 
+
+Console.WriteLine("Enter a number to count up to: ");
+int countUpNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Numbers from 1 to: " + countUpNumber + ":");
+
+for (int i = 1; i <= countUpNumber; i++)
+{
+    Console.Write(i + " "); 
+}
+
+Console.WriteLine();
+
+// Count down to 
+
+Console.WriteLine("Enter a number to count down to: ");
+int countDownNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Number from " + countDownNumber + " down to 1:");
+
+for(int i = countDownNumber; i >= 1; i--)
+{
+    Console.Write(i + " ");
+}
+
+Console.WriteLine();
+
+// Print all even numbers from 2 up to user input
+
+Console.WriteLine("Enter a number to print even number up to: ");
+int evenUpToNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Even numbers from 2 up to " + evenUpToNumber + ":");
+
+for (int i = 2; i <= evenUpToNumber; i += 2)
+{
+    Console.Write(i + " "); 
+}
+
+Console.WriteLine();
+
+// Print all odd numbers from 1 to user input
+
+Console.WriteLine("Enter a number to print odd numbers up to: ");
+int oddUpToNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Odd numbers from 1 up to " + oddUpToNumber + ":");
+
+for (int i = 1; i <= oddUpToNumber; i += 2)
+{
+    Console.Write(i + " "); 
+}
+Console.WriteLine();
+
+#endregion
