e8a6c28d47cd29f1f09241d14f4a4d287e2412d2
Animrymic <trojanovikdragan@gmail.com>
2026-03-26 19:11:30 +0100

    demo code added

--- Class04.Hworks/Class04/Class04.Homeworks/Program.cs
+++ Class04.Hworks/Class04/Class04.Homeworks/Program.cs
@@ -1,4 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿
+
+
+static void Main()
+{
+    Console.Write("Enter your birthdate (yyyy-mm-dd): ");
+    DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+    int age = AgeCalculator(birthDate);
+
+    Console.WriteLine("Your age is: " + age);
+}
+
+static int AgeCalculator(DateTime birthDate)
+{
+    DateTime today = DateTime.Today;
+
+    int age = today.Year - birthDate.Year;
+
+    // If birthday hasn't happened yet this year, subtract 1
+    if (today.Month < birthDate.Month ||
+       (today.Month == birthDate.Month && today.Day < birthDate.Day))
+    {
+        age--;
+    }
+
+    return age;
+}
