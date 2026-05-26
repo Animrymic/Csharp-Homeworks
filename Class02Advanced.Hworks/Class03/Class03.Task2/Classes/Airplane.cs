3e76b0a6714e93bfdf4142128fe5436ff70bf352
Animrymic <trojanovikdragan@gmail.com>
2026-05-26 18:33:18 +0200

    Class03 homework added

---
+++ Class02Advanced.Hworks/Class03/Class03.Task2/Classes/Airplane.cs
@@ -0,0 +1,10 @@
+﻿namespace Class03.Task2.Classes;
+
+public class Airplane : Vehicle
+{
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"I am an airplane and I do have some wheels, I also can fly in the sky!");
+    }
+    
+}
---
+++ Class02Advanced.Hworks/Class03/Class03.Task2/Classes/Vehicle.cs
@@ -0,0 +1,9 @@
+﻿namespace Class03.Task2.Classes;
+
+public class Vehicle
+{
+    public virtual void DisplayInfo()
+    {
+        Console.WriteLine("This is a vehicle.");
+    }
+}
