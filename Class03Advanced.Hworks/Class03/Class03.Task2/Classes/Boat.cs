3b957c24cbe4156df23642aeddf0f21f64bd4646
Animrymic <trojanovikdragan@gmail.com>
2026-05-26 18:35:02 +0200

    class03 homeworks added

---
+++ Class03Advanced.Hworks/Class03/Class03.Task2/Classes/Boat.cs
@@ -0,0 +1,9 @@
+﻿namespace Class03.Task2.Classes;
+
+public class Boat : Vehicle
+{
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"I am a boat and I do not have wheels, I go on water!"); 
+    }
+}
---
+++ Class03Advanced.Hworks/Class03/Class03.Task2/Classes/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace Class03.Task2.Classes;
+
+public class Car : Vehicle
+{
+    public override void DisplayInfo()
+    {
+        Console.WriteLine("I am a car, and I can drive on 4 wheels");
+    }
+    
+}
